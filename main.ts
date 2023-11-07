3ef7eaefd3a21d8f25d9b2df03236ea7ea75dbd2
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2023-11-07 10:42:42 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,7 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    t1 = input.runningTimeMicros() / 1000000
-    d1 = receivedNumber / 6 * 9.56 / 100
-    Vitesse = Math.abs(d2 - d1) / Math.abs(t2 - t1)
-    serial.writeValue("vi", Vitesse)
-    t2 = t1
-    d2 = d1
+    Temps = input.runningTime() / 1000
+    Distance = receivedNumber / 20 * 15.7 / 100
+    serial.writeNumbers([Temps, Math.round(Distance * 100) / 100])
     led.toggle(0, 0)
 })
 input.onButtonPressed(Button.A, function () {
@@ -14,13 +11,9 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(1000)
     control.reset()
 })
-let t2 = 0
-let d2 = 0
-let Vitesse = 0
-let d1 = 0
-let t1 = 0
+let Distance = 0
+let Temps = 0
 radio.setGroup(1)
-let Fentes = 0
-t1 = 0
-d1 = 0
+Temps = 0
+Distance = 0
 basic.showIcon(IconNames.Yes)
