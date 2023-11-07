c09e299daad37a81ff761ba6a8269ff4b8d327fc
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2023-11-07 12:44:42 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    Temps = input.runningTime() / 1000
+    if (receivedNumber >= 1 && Debut == 2) {
+        t1 = input.runningTime()
+    }
+    Temps = (input.runningTime() - t1) / 1000
     Distance = receivedNumber / 20 * 15.7 / 100
-    serial.writeNumbers([Temps, Math.round(Distance * 100) / 100])
+    serial.writeNumbers([Math.round(Distance * 100) / 100, Temps])
     led.toggle(0, 0)
+    Debut = 3
 })
 input.onButtonPressed(Button.A, function () {
     radio.sendString("Go")
@@ -11,9 +15,13 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(1000)
     control.reset()
 })
+let t1 = 0
+let Debut = 0
 let Distance = 0
 let Temps = 0
 radio.setGroup(1)
 Temps = 0
 Distance = 0
+Debut = 0
 basic.showIcon(IconNames.Yes)
+Debut = 2
